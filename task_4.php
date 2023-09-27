a54e845ee4f83cbc3118810223dc1abcf503a44f
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2023-09-27 21:47:49 +0600

    refactor: add grade_point parameters
    Signed-off-by: Jubayer Abdullah Joy <jubayerjoy98@gmail.com>

--- task_4.php
+++ task_4.php
@@ -3,11 +3,11 @@
 ## Task 4: Multidimensional Array
 // Create a multidimensional array called $studentGrades to store the grades of three students.
 // Each student has grades for three subjects: Math, English, and Science. Write a PHP function which takes "$studentGrades" as an argument to calculate and print the average grade for each student.
-
+// Should print A+, A, A-, B, C, F depending on the average grade.
 
 $studentGrades = [
     [
-        'Math' => 85,
+        'Math' => 100,
         'English' => 92,
         'Science' => 78
     ],
@@ -17,16 +17,42 @@ $studentGrades = [
         'Science' => 76
     ],
     [
-        'Math' => 92,
-        'English' => 85,
-        'Science' => 84
+        'Math' => 42,
+        'English' => 45,
+        'Science' => 30
     ]
 ];
 
-function calculateAndPrintAverageGrade($studentGrades) {
+$grades = [
+    'A+' => 80,
+    'A' => 70,
+    'A-' => 60,
+    'B' => 50,
+    'C' => 40,
+    'F' => 0
+];
+
+function calculateAverageGrade($studentGrades) {
+    $studentAverageGrades = [];
+
     foreach ($studentGrades as $student => $grades) {
         $averageGrade = array_sum($grades) / count($grades);
-        echo "Student " . ($student + 1) . " has obtained average grade: " . $averageGrade . "\n";
+        $studentAverageGrades[$student] = $averageGrade;        
+    }
+    return $studentAverageGrades;
+}
+
+function calculateAndPrintAverageGrade($studentGrades) {
+    $studentAverageGrades = calculateAverageGrade($studentGrades);
+    global $grades;
+
+    foreach ($studentAverageGrades as $student => $averageGrade) {
+        foreach ($grades as $grade => $minGrade) {
+            if ($averageGrade >= $minGrade) {
+                echo "$student: $grade\n";
+                break;
+            }
+        }
     }
 }
 
