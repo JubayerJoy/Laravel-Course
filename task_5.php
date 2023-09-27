61033f72a5bafbcaa0cd16fe40bc6d546be03667
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2023-09-27 21:32:56 +0600

    fix: add one-type-of-char_js_must rule
    Signed-off-by: Jubayer Abdullah Joy <jubayerjoy98@gmail.com>

--- task_5.php
+++ task_5.php
@@ -4,15 +4,32 @@
 // Create a PHP function called generatePassword ($length) that generates a random password of the specified length.
 // The password should include lowercase letters, uppercase letters, numbers, and special characters (!@#$%^&*()_+).
 // Write a PHP program to generate a password with a length of 12 characters using this function and print the password.
+// It should have atleast one uppercase, one lowercase, one number and one special character.
 
 function generatePassword($length) {
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $specialChars = '!@#$%^&*()_+';
+
+    $allChars = $uppercase . $lowercase . $numbers . $specialChars;
+
     $password = '';
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    $charactersLength = strlen($characters);
-    
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, $charactersLength - 1)];
+
+    $password .= $uppercase[rand(0, strlen($uppercase) - 1)];
+    $password .= $lowercase[rand(0, strlen($lowercase) - 1)];
+    $password .= $numbers[rand(0, strlen($numbers) - 1)];
+    $password .= $specialChars[rand(0, strlen($specialChars) - 1)];
+
+    $passwordLength = $length - 4;
+
+    for ($i = 0; $i < $passwordLength; $i++) {
+        $password .= $allChars[rand(0, strlen($allChars) - 1)];
     }
+
+    $passwordArray = str_split($password);
+    shuffle($passwordArray);
+    $password = implode('', $passwordArray);
     return $password;
 }
 
