ed6b39bc9f6053fcee99b800d019bbd9e063300b
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2023-10-31 23:41:12 +0600

    feat: create file structure
    Signed-off-by: Jubayer Abdullah Joy <jubayerjoy98@gmail.com>

---
+++ components/header.php
@@ -0,0 +1,17 @@
+<!-- header.php -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Our Crew Project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
