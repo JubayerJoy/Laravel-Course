a7c8cf95fd3b355e580f60005c06be332729195b
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2023-11-11 22:05:23 +0600

    chore: remove old file

--- task1.sql
+++
@@ -1,6 +0,0 @@
--- Task 1: Retrieve customer information along with the total number of orders placed by each customer
-SELECT Customers.name, COUNT(Orders.order_id) AS total_orders
-FROM Customers
-LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
-GROUP BY Customers.customer_id, Customers.name
-ORDER BY total_orders DESC;
--- task2.sql
+++
@@ -1,5 +0,0 @@
--- Retrieve product name, quantity, and total amount for each order item
-SELECT Products.name AS product_name, Order_Items.quantity, (Order_Items.quantity * Order_Items.unit_price) AS total_amount
-FROM Order_Items
-JOIN Products ON Order_Items.product_id = Products.product_id
-ORDER BY Order_Items.order_id ASC;
